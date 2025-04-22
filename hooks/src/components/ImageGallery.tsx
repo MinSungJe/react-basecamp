0493c224e2591113c66543ddb5bfd97fc33d96c2
Minje Sung <minje813@dgu.ac.kr>
2025-04-22 17:03:14 +0900

    feat(hooks): useEffect의 기본 개념 이해하기

--- hooks/src/components/ImageGallery.tsx
+++ hooks/src/components/ImageGallery.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 const API_KEY = import.meta.env.VITE_PIXABAY_API_KEY;
+const BASE_URL = 'https://pixabay.com/api/';
 
 interface Image {
   id: number;
@@ -10,7 +11,7 @@ interface Image {
 
 const ImageGallery: React.FC = () => {
   const [images, setImages] = useState<Image[]>([]);
-  const [keyword, setKeyword] = useState("");
+  const [keyword, setKeyword] = useState('');
 
   // useEffect를 사용하여 keyword가 변경될 때마다 이미지를 가져오는 로직을 작성해보세요.
   useEffect(() => {
@@ -20,6 +21,24 @@ const ImageGallery: React.FC = () => {
     // 2. fetchImages 함수 내부에서 Pixabay API를 호출하여 이미지를 가져오세요.
     // 3. 가져온 이미지를 상태(images)에 업데이트하세요.
     // 4. keyword가 존재할 때만 fetchImages 함수를 호출하세요.
+    const params = new URLSearchParams({
+      key: API_KEY,
+      q: keyword,
+      image_type: 'photo',
+    });
+
+    const fetchImages = async () => {
+      console.log(BASE_URL + '?' + params.toString());
+      try {
+        const response = await fetch(BASE_URL + '?' + params.toString());
+        const data = await response.json();
+        setImages(data.hits);
+      } catch (error) {
+        if (error instanceof Error) throw new Error(error.message);
+      }
+    };
+
+    if (keyword !== '') fetchImages();
   }, [keyword]);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -35,12 +54,7 @@ const ImageGallery: React.FC = () => {
       <section>
         <form onSubmit={handleSubmit}>
           <label htmlFor="searchKeyword">검색:</label>
-          <input
-            type="text"
-            id="searchKeyword"
-            name="keyword"
-            placeholder="키워드 입력"
-          />
+          <input type="text" id="searchKeyword" name="keyword" placeholder="키워드 입력" />
           <button type="submit">Search</button>
         </form>
       </section>
